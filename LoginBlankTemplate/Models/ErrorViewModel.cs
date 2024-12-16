987fbb0443123f3a1fa2dd2c3b6cbd9a68a5415a
Luke Thompson <luke@macss.co.uk>
2024-12-16 14:42:24 +0000

    Add project files.

---
+++ LoginBlankTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginBlankTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
